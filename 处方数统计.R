fdf8d5aa6e01ea450daa8d58642d22df166e8bb7
lwt578 <lwt578@126.com>
2025-03-16 16:51:38 +0800

    2025.3.16

--- 处方数统计.R
+++ 处方数统计.R
@@ -30,7 +30,7 @@ ggplot(df_stat,aes(时间,每月处方数))+
                    )+
   
   theme_classic()+
-  labs(title = "2018-2024年每月处方数")+
+  labs(title = "2018-2025年每月门诊处方数")+
   theme(plot.title = element_text(hjust = 0.5,size=18))
 
 ggplot(df_stat,aes(时间,月销售金额))+
@@ -44,7 +44,7 @@ ggplot(df_stat,aes(时间,月销售金额))+
                    date_minor_breaks = "1 month"
   )+
   theme_classic()+
-  labs(title = "2018-2024年每月药品销售金额")+
+  labs(title = "2018-2025年每月门诊药品销售金额")+
   theme(plot.title = element_text(hjust = 0.5,size=18))
 
   
@@ -66,21 +66,21 @@ df_stat|>
             vjust = 0,nudge_y = 200,size=3)+
   scale_x_continuous(name="月",
                      breaks = c(1:12))+
-  labs(title = "2024年每月药品处方数")+
+  labs(title = "2024年每月门诊药品处方数")+
   theme(legend.position = "none",
         plot.title = element_text(hjust = 0.5,size=18))
 
 
 df_stat|>
-  filter(月==11)|>
+  filter(月==2)|>
   ggplot(aes(年,每月处方数,fill=每月处方数))+
   geom_col()+
   geom_text(aes(label=每月处方数),
             vjust = 0,nudge_y = 200)+
   scale_x_continuous(name="年",
-                     breaks = c(2018:2024))+
+                     breaks = c(2018:2025))+
   theme_classic()+
-  labs(title = "2018-2024年11月份处方数对比",
+  labs(title = "2018-2025年2月份门诊处方数对比",
        y = "处方数")+
   theme(plot.title = element_text(hjust = 0.5,size=18))+
   theme(legend.position = "none")+
